bf815bda0212a415bcabc107c7a05cfb9891bb8d
Gustavo Sabino <gustavo.sabino@hotmail.com>
2016-11-18 14:38:35 -0200

    Adding classifier module and changes in view

--- detector/views.py
+++ detector/views.py
@@ -3,11 +3,13 @@ from django.views.generic import View
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from ImageCategory import _grab_probability
 import numpy as np
 import urllib
 import json
 import cv2
 import os
+
 # Create your views here.
 
 class FaceDetect(View):
@@ -22,24 +24,37 @@ class FaceDetect(View):
 
 		output = dict()
 
-		url = request.POST["url"]
+		urls = request.POST.getlist("url")
 
-		response = urllib.urlopen(url)
-		data = response.read()
+		faces = list()
 		
-		image = np.asarray(bytearray(data),dtype="uint8")
-		image = cv2.imdecode(image,cv2.IMREAD_COLOR)
-		image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-		detector = cv2.CascadeClassifier('/root/Opencv/data/haarcascades/haarcascade_frontalface_default.xml')
+		for url in urls:
+			response = urllib.urlopen(url)
+			data = response.read()
+			
+			image = np.asarray(bytearray(data),dtype="uint8")
+			image = cv2.imdecode(image,cv2.IMREAD_COLOR)
+			image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+			detector = cv2.CascadeClassifier('/root/Opencv/data/haarcascades/haarcascade_frontalface_default.xml')
 
 
-		rects = detector.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5,
-			minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+			rects = detector.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5,
+				minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+			
+			rects = [(int(x), int(y), int(x + w), int(y + h)) for (x, y, w, h) in rects]
+
+			
+			output['num_faces']=len(rects)
+			output['faces'] = rects
+			output['sucess'] = True
+
+			
+
+			output['probability_response'] = _grab_probability(url)
+
+			faces.append(output)
+
+
 		
-		rects = [(int(x), int(y), int(x + w), int(y + h)) for (x, y, w, h) in rects]
 
-		output['num_faces']=len(rects)
-		output['faces'] = rects
-		output['sucess'] = True
-
-		return JsonResponse(output)
+		return JsonResponse(output=faces)
